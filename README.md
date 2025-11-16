6b42ee696184ba492c0659c1ea11b1f4c154363a
YNSJK <yaniskla3a@gmail.com>
2025-11-16 02:16:22 +0100

    adds backup file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # simple-backup-script
-Simple TAR backup script
+
+A small, single-file Bash backup helper that creates a tarball of a given source directory.
+
+**What the script does**
+- Creates a tar archive named `backup.tar` in `SOURCE_DIR` that contains the files from `SOURCE_DIR`.
+- Calculates the MD5 checksum of the newly created tarball.
+- If a tarball already exists at `DEST_DIR/backup.tar`, it calculates the MD5 checksum of that file and compares it to the local checksum.
+	- If checksums differ, it copies the new tarball to `DEST_DIR` (overwriting the existing backup).
+	- If checksums are the same, it does nothing (avoids unnecessary copy).
+- If no tarball exists at `DEST_DIR/backup.tar`, it copies the new tarball there.
+- Removes the local `SOURCE_DIR/backup.tar` after handling the copy.
+- Appends informational and error messages to the file defined by `PATH_TO_LOG`.
+
+**Usage**
+1. Edit `backup-git-repos.sh` and set the three variables at the top to valid paths:
+
+```bash
+PATH_TO_LOG="/path/to/log"
+SOURCE_DIR="/path/to/source/dir"
+DEST_DIR="/path/to/backup"
+```
+
+2. Make the script executable (optional) and run it:
+```bash
+chmod +x backup-script.sh
+./backup-script.sh
+# or
+bash backup-script.sh
+```
---
+++ backup-script.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+###################
+### Global Vars ###
+###################
+
+PATH_TO_LOG="/path/to/log"
+SOURCE_DIR="/path/to/source/dir"
+DEST_DIR="/path/to/backup"
+
+########################
+### Helper Functions ###
+########################
+
+# $1 = message
+error_exit()
+{
+    log_append "ERROR" $1
+    exit 1
+}
+
+# $1 = log level
+# $2 = message
+log_append()
+{
+    time_stamp=$(date +"%d.%m.%Y %H:%M:%S")
+    echo -e "$time_stamp\t$1\t$2" >> $PATH_TO_LOG
+}
+
+###################
+### Main Script ###
+###################
+
+# create new tar
+log_append "INFO" "Tryingk to create a new tar file at $SOURCE_DIR/backup.tar"
+tar -cf $SOURCE_DIR/backup.tar -C $SOURCE_DIR $SOURCE_DIR/* ||  error_exit "Could not create tar file at $SOURCE_DIR/backup.tar"
+log_append "INFO" "Successfully created a new tar file at $SOURCE_DIR/backup.tar"
+
+# md5sum new tar
+md5sum_local=$(md5sum $SOURCE_DIR/backup.tar | awk '{ print $1 }')
+log_append "INFO" "MD5SUM of local tar is $md5sum_local"
+
+#check if destination file is present
+if [ -e $DEST_DIR/backup.tar ]; then
+    md5sum_remote=$(md5sum $DEST_DIR/backup.tar | awk '{ print $1 }')
+    log_append "INFO" "MD5SUM of remote tar is $md5sum_remote"
+
+    if [ "$md5sum_local" != "$md5sum_remote" ]; then
+        log_append "INFO" "MD5SUMs differ, copying new tar to $DEST_DIR"
+        cp $SOURCE_DIR/backup.tar $DEST_DIR/backup.tar || error_exit "Could not copy new tar to $DEST_DIR"
+        log_append "INFO" "Successfully copied new tar to $DEST_DIR"
+    else
+        log_append "INFO" "MD5SUMs are the same, no need to copy new tar to $DEST_DIR"
+    fi
+else
+    log_append "INFO" "Remote tar does not exist... copying new tar to $DEST_DIR"
+    cp $SOURCE_DIR/backup.tar $DEST_DIR/backup.tar || error_exit "Could not copy new tar to $DEST_DIR"
+    log_append "INFO" "Successfully copied new tar to $DEST_DIR"
+fi
+
+# remove local tar
+log_append "INFO" "Removing local tar at $SOURCE_DIR/backup.tar"
+rm $SOURCE_DIR/backup.tar || error_exit "Could not remove local tar at $SOURCE_DIR/backup.tar"
+log_append "INFO" "Successfully removed local tar at $SOURCE_DIR/backup.tar"
+
+exit 0
