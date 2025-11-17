8e08239f57fc468d4a80eb6e5e8775a69e30d6a5
YNSJK <yaniskla3a@gmail.com>
2025-11-17 19:01:23 +0100

    adds support for multiple destination paths

--- backup-script.sh
+++ backup-script.sh
@@ -7,6 +7,7 @@
 PATH_TO_LOG="/path/to/log"
 SOURCE_DIR="/path/to/source/dir"
 DEST_DIR="/path/to/backup"
+declare -a DEST_DIRS=("/path/to/backup1" "/path/to/backup2")
 
 ########################
 ### Helper Functions ###
@@ -41,26 +42,32 @@ md5sum_local=$(md5sum $SOURCE_DIR/backup.tar | awk '{ print $1 }')
 log_append "INFO" "MD5SUM of local tar is $md5sum_local"
 
 #check if destination file is present
-if [ -e $DEST_DIR/backup.tar ]; then
-    md5sum_remote=$(md5sum $DEST_DIR/backup.tar | awk '{ print $1 }')
-    log_append "INFO" "MD5SUM of remote tar is $md5sum_remote"
+for DEST_DIR in "${DEST_DIRS[@]}"; do
+    log_append "INFO" "Processing destination directory: $DEST_DIR"
 
-    if [ "$md5sum_local" != "$md5sum_remote" ]; then
-        log_append "INFO" "MD5SUMs differ, copying new tar to $DEST_DIR"
+    if [ -e $DEST_DIR/backup.tar ]; then
+        md5sum_remote=$(md5sum $DEST_DIR/backup.tar | awk '{ print $1 }')
+        log_append "INFO" "MD5SUM of remote tar ($DEST_DIR) is $md5sum_remote"
+
+        if [ "$md5sum_local" != "$md5sum_remote" ]; then
+            log_append "INFO" "MD5SUMs differ, copying new tar to $DEST_DIR"
+            cp $SOURCE_DIR/backup.tar $DEST_DIR/backup.tar || error_exit "Could not copy new tar to $DEST_DIR"
+            log_append "INFO" "Successfully copied new tar to $DEST_DIR"
+        else
+            log_append "INFO" "MD5SUMs are the same, no need to copy new tar to $DEST_DIR"
+        fi
+    else
+        log_append "INFO" "Remote tar does not exist... copying new tar to $DEST_DIR"
         cp $SOURCE_DIR/backup.tar $DEST_DIR/backup.tar || error_exit "Could not copy new tar to $DEST_DIR"
         log_append "INFO" "Successfully copied new tar to $DEST_DIR"
-    else
-        log_append "INFO" "MD5SUMs are the same, no need to copy new tar to $DEST_DIR"
     fi
-else
-    log_append "INFO" "Remote tar does not exist... copying new tar to $DEST_DIR"
-    cp $SOURCE_DIR/backup.tar $DEST_DIR/backup.tar || error_exit "Could not copy new tar to $DEST_DIR"
-    log_append "INFO" "Successfully copied new tar to $DEST_DIR"
-fi
+done
 
 # remove local tar
 log_append "INFO" "Removing local tar at $SOURCE_DIR/backup.tar"
 rm $SOURCE_DIR/backup.tar || error_exit "Could not remove local tar at $SOURCE_DIR/backup.tar"
 log_append "INFO" "Successfully removed local tar at $SOURCE_DIR/backup.tar"
+
+log_append "SUCCESS" "Backup script completed successfully"
 
 exit 0
